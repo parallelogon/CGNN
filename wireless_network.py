8ed54987be2e1739318564239dae4316c5ce3070
Notmyname <Wrong@example.com>
2021-10-15 12:38:19 +0200

    added new project, wireless_network.py

---
+++ wireless_network.py
@@ -0,0 +1,274 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+from scipy.spatial import distance_matrix
+
+
+"""
+ Channel class, this is a test class for a later implementation.
+ Not ultimately used.
+
+class Channel():
+    N0 = 10**-6
+
+    def __init__(self, d0, gamma):
+        self.d0 = d0
+        self.gamma = gamma
+
+    def pathloss(self,d, **kwargs):
+
+        if len(kwargs.keys()) == 0:
+            return (self.d0/d)**self.gamma
+        else:
+            assert 's' in kwargs.keys() and 'Q' in kwargs.keys()
+            s = kwargs['s']; Q = kwargs['Q'];
+            
+            L = max(d.shape)
+            d = d.reshape((1,-1))
+            return (self.d0/d)**self.gamma * torch.Tensor(Q,L).exponential_(s)/s #torch.random.exponential(s, size = (Q,L))/s
+
+    def c(self, p,h):
+        assert len(p) == len(h)
+        return torch.log(1 + p*h/self.N0)
+
+channel = Channel(1, 2.2)
+
+d = torch.arange(1,100)
+
+fig1 = plt.figure()
+plt.plot(d,torch.log(channel.pathloss(d)))
+fig1.show()
+
+realization = channel.pathloss(d, s = 2, Q = 100)
+
+fig2 = plt.figure()
+plt.plot(d.numpy(), (torch.max(realization, axis = 0))[0].numpy())
+plt.plot(d.numpy(), torch.min(realization, axis = 0)[0].numpy())
+fig2.show()
+
+high = []
+low = []
+for dist in range(len(d)):
+    p = 0.05*torch.ones(realization[dist,:].shape)
+    quality = channel.c(p, realization[dist,:])
+    high.append(torch.max(quality))
+    low.append(torch.min(quality))
+
+fig3 = plt.figure()
+plt.plot(d,high)
+plt.plot(d,low)
+plt.show()
+"""
+
+
+class Net():
+
+    """
+    Net() class which holds all the information of a wireless network with area
+    wx*wy, max receiver distance wc, and density of transmitters/receivers rho
+    calculate the power transmitted gives a pathloss_matrix (h) s.t. h_{i,j} is
+    the power transmitted from transmitter i to receiver j.  Sampled Q times,
+    the power transmitted is assumed to follow an exp. distribution
+    with factors d0 and gamma found experimentally
+    """
+
+    def __init__(self, wx, wy, wc, rho):
+        n = int(wx*wy*rho)
+        self.n = n
+        self.transmitters = torch.hstack(
+            (torch.Tensor(n, 1).uniform_(0, wx), torch.Tensor(n, 1).uniform_(0, wy)))
+
+        theta = torch.Tensor(n, 1).uniform_(0., 2*np.pi)
+
+        self.receivers = self.transmitters + \
+            torch.Tensor(n, 1).uniform_(0, wc) * \
+            (torch.hstack((torch.cos(theta), torch.sin(theta))))
+
+    def pathloss_matrix(self, d0, gamma):
+        self.d0 = d0
+        self.gamma = gamma
+
+        a = self.transmitters
+        b = self.receivers
+        d = torch.linalg.norm(a[:, None, :] - b[None, :, :], axis=-1)
+
+        self.d = d
+        self.H = normalize_matrix((d0/d)**gamma)
+        return self
+
+    def sample(self, s, Q):
+        self.samples = self.H[None, :, :] * \
+            torch.Tensor(Q, self.n, self.n).exponential_(s)/s
+        return self
+
+# Helper function to stabilize training
+
+
+def normalize_matrix(A):
+    eigenvalues = np.linalg.eigvals(A)
+    return A / np.max(eigenvalues.real)
+
+
+# Channel capacity is quantity to be maximized
+def capacity(Q_nets, P, N0=1e-6):
+
+    # project columnQ_nets of P onto
+    BATCHSIZE, n, _ = Q_nets.shape
+
+    # extract diagonals of hp
+    num = torch.diagonal(Q_nets, dim1=-2, dim2=-1)*(P.squeeze())
+
+    # subtract diagonals from matrix multiply transmitter j on receiver i
+    den = torch.einsum('ijk,ijl->ikl', Q_nets, P).squeeze() - num + N0
+    return torch.log(1 + num/den)
+
+
+# net samples should inheret attributes from Net() and the following parameters
+# determine the size of the area / number of nodes in the graph
+wx = 200
+wy = 100
+wc = 50
+rho = 0.005
+n = int(rho*wx*wy)
+net = Net(wx, wy, wc, rho)
+d0 = 1
+gamma = 2.2
+s = 2
+
+# Batchsize, generate samples on the fly
+BATCHSIZE = 100
+
+# graphs the net with black lines connecting receivers and transmitters
+fig3 = plt.figure()
+
+plt.scatter(net.receivers[:, 0], net.receivers[:, 1], c='red')
+plt.scatter(net.transmitters[:, 0], net.transmitters[:, 1], c='blue')
+
+for i in range(net.receivers.shape[0]):
+    plt.plot([net.transmitters[i, 0], net.receivers[i, 0]],
+             [net.transmitters[i, 1], net.receivers[i, 1]], c='black', lw=.5)
+
+plt.show()
+
+
+class Layer(nn.Module):
+    def __init__(self, k, Fin, Fout, sigma):
+        super(Layer, self).__init__()
+
+        self.k = k
+
+        A = torch.rand(k, Fin, Fout)
+        self.A = nn.parameter.Parameter(A)
+
+        self.Fin = Fin
+        self.Fout = Fout
+
+        self.sigma = sigma
+
+    def forward(self, H, x):
+        z = self.apply_filter(H, x, self.A)
+        x = self.sigma(z)
+        return x
+
+    def apply_filter(self, H, X, A):
+        k, _, _ = A.shape
+
+        u = 0
+        for i in range(k):
+            HZA = X @ A[i, :, :]
+            u = u + HZA
+            X = torch.einsum('ijk,ikl->ijl', H, X)
+
+        return u
+
+
+class REGNN(nn.Module):
+    def __init__(self, K, F, sigma=lambda x: x):
+        super(REGNN, self).__init__()
+
+        layers = []
+
+        assert len(K) == len(F) - 1, "Incorrect number of layers"
+
+        for layer in range(len(K)):
+            layers.append(Layer(K[layer], F[layer], F[layer+1], sigma))
+
+        # cannot use sequential, as it only allows one parameter input
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, H, x):
+        for layer in self.layers:
+            x = layer(H, x)
+        return x
+
+# negative quantity to be maximized, upper bounded at zero
+
+
+def loss(Hh, P, mu):
+    return -capacity(Hh, P).mean() + mu*P.mean()
+
+
+net.pathloss_matrix(d0, gamma)
+
+
+# Initialize the model (R)andom (E)dge (G)raphical (N)eural (N)etwork
+F = [1, 1, 1, 1, 1, 1]
+K = [5, 5, 5, 5, 5]
+H = net.sample(s, BATCHSIZE).samples
+filter = REGNN(K, F, nn.ReLU())
+
+# Hyperparameters
+LR = 0.05  # learning rate
+EPOCHS = 10
+N_REALIZATIONS = 10**4  # number of samples total per epoch
+N_BATCHES = N_REALIZATIONS//BATCHSIZE
+MU = 0.01  # penalty on total power allocated
+LOSS_TRAIN = []
+
+# Initalize the network
+net = Net(wx, wy, wc, rho).pathloss_matrix(d0, gamma)
+
+# Use adam optimizer with base values for betas
+optimizer = optim.Adam(filter.parameters(), LR)
+
+for epoch in range(EPOCHS):
+    print("")
+    print("Epoch %d" % (epoch+1))
+
+    for batch in range(N_BATCHES):
+
+        # sample BATCHSIZE randomly generated samples from the random net
+        H = net.sample(s, BATCHSIZE).samples[:]
+        P = torch.ones(BATCHSIZE, net.n, 1)
+
+        if batch % 20 == 0:
+            print("")
+            print("    (E: %2d, B: %3d)" % (epoch+1, batch+1), end=' ')
+            print("")
+
+        # zero gradient
+        filter.zero_grad()
+
+        # power values
+        P = filter(H, P)
+
+        # loss value
+        lval = loss(H, P, MU)
+
+        # deriv
+        lval.backward()
+
+        # optimize
+        optimizer.step()
+
+        # track
+        LOSS_TRAIN += [lval.item()]
+
+        if batch % 20 == 0:
+            print("\t Filter: %6.4f [T]" % lval)
+print("")
+
+fig1 = plt.figure()
+plt.plot(LOSS_TRAIN)
+plt.show()
